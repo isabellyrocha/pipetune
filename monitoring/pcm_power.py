f5cbcad7901785f70838a0ec888923a77fe538ad
Isabelly Rocha <isbaelly.rocha@unine.ch>
2020-07-01 15:53:28 +0000

    Refactoring PCM power collector.

--- monitoring/pcm_power.py
+++ monitoring/pcm_power.py
@@ -4,6 +4,7 @@ import sys
 from datetime import datetime
 from influxdb import InfluxDBClient
 import time 
+import argparse
 
 def get_json(nodename, timestamp, power):
     json_body = [{
@@ -25,34 +26,37 @@ def next(file_name):
         time.sleep(1)
     return line
 
-#log_file = open("/home/ubuntu/sprinting/monitoring/eiger-2.log", "r")
-file_name = sys.argv[1]
-log_file = open(file_name, "r")
-node = sys.argv[2]
-line = next(log_file)
+def main(file_name, node):
+    log_file = open(file_name, "r")
+    line = next(log_file)
 
-while not line.startswith("Date"):
-   line = next(log_file)
-
-sline = line.split(",")
-for i in range(len(sline)):
-    if sline[i] == "Proc Energy (Joules)":
-        eId = i
-        break
-
-client = InfluxDBClient('localhost', 8086, 'root', 'root', 'energy')
-while True:
-    try:
+    while not line.startswith("Date"):
         line = next(log_file)
-        print(line)
-        sline = line.split(",")
-        date = sline[0]
-        print(sline[1])
-        t = sline[1].split(".")[0]
-        dt = datetime.strptime("%s %s" % (date, t), "%Y-%m-%d %H:%M:%S")
-        #timestamp = int(datetime.timestamp(dt))
-        power = sline[eId]
-        json_body = get_json(node, dt, power)
-        client.write_points(json_body)
-    except Exception:
-        print("Exception has occured. Restarting...")
+
+    sline = line.split(",")
+    for i in range(len(sline)):
+        if sline[i] == "Proc Energy (Joules)":
+            eId = i
+            break
+
+    client = InfluxDBClient('localhost', 8086, 'root', 'root', 'energy')
+    while True:
+        try:
+            line = next(log_file)
+            sline = line.split(",")
+            date = sline[0]
+            t = sline[1].split(".")[0]
+            dt = datetime.strptime("%s %s" % (date, t), "%Y-%m-%d %H:%M:%S")
+            power = sline[eId]
+            json_body = get_json(node, dt, power)
+            client.write_points(json_body)
+        except Exception:
+            print("Exception has occured. Restarting...")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("fileName", type=str, help="The file with the PCM measurements.")
+    parser.add_argument("nodeName", type=str, help="The name of the node where the measurements are collected.")
+    args = parser.parse_args()
+
+    main(args.fileName, args.nodeName)
