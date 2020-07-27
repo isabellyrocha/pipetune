3f7496875f0ab4bb253e0f454466cb0acaa5d85c
Isabelly Rocha <isbaelly.rocha@unine.ch>
2020-07-27 20:57:44 +0000

    Cleaning up clustering.

--- clustering/offline/filter-results.sh
+++ clustering/offline/filter-results.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python3 filter_spark_logs.py > spark_filtered.log
-scp eiger-2.maas:perf/* /home/ubuntu/perf
-python3 filter_perf_logs.py > perf_filtered.log
-python3 agg_per_epoch.py > agg_per_epoch.log
-python3 agg_per_event.py > agg_per_event.log
+python3 filter_spark_logs.py > data/spark_filtered.log
+scp eiger-2.maas:perf/* data/perf
+python3 filter_perf_logs.py > data/perf_filtered.log
+python3 agg_per_epoch.py > data/agg_per_epoch.log
+python3 agg_per_event.py > data/agg_per_event.log
