c6b8cbf6feeb519e314190cbafd00791e74106a4
Isabelly Rocha <isbaelly.rocha@unine.ch>
2020-07-27 20:40:20 +0000

    Cleaning up clusting.

--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -12,23 +12,14 @@ clusters = 4
 events = {}
 config = []
 
-with open('../experiments/agg_per_event.log') as fp:
+with open('../offline/data/agg_per_event.log') as fp:
     line = fp.readline()
     while line:
         sline = line.split(",")
-        model = sline[0]
-        dataset = sline[1]
-        cores = sline[2]
-        memory = sline[3]
-        batch = sline[4]
-        #log_id = sline[5]
-        event = sline[5]
-        count = float(sline[6])
-        duration = float(sline[7].strip())
+        (model, dataset, cores, memory, batch, event, count, duration) = line.strip().split(",")
         name = "%s,%s,%s,%s,%s,%d" % (model, dataset, cores, memory, batch, duration)
         if event not in events:
             events[event] = []
-        #if int(phase) < 5 and int(phase) > 0:
         events[event].append(count)
         if name not in config:
             config.append(name)
@@ -41,20 +32,16 @@ kmeans = model.fit(df)
 
 joblib.dump(model, 'model.pkl')  
 model_loaded = joblib.load('model.pkl')
-
 print(model_loaded)
-
 
 for i in range(len(config)):
     print("%s,%d" % (config[i], kmeans.labels_[i]))
 
-# get test
 test = {}
 for event in list(events.keys()):
     test[event] = [events[event][0]]
 print(test)
 df_test = DataFrame(test,columns=list(test.keys()))
-
 
 prediction = model.predict(df_test)
 print(prediction)
