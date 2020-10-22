46c93d6b251795d15d228b97cee1c077b0a4e387
isabellyrocha <isabelly.rocha@unine.ch>
2020-10-22 20:31:53 +0000

    Updating scripts.

--- apps/news20/run-news20.sh
+++ apps/news20/run-news20.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+MASTER=spark://eiger-7.maas:7077
+
 spark-submit \
-	--master spark://eiger-1.maas:7077 \
+	--master $MASTER \
 	--driver-memory 32G \
 	--total-executor-cores $1 \
 	--executor-cores $2 \
 	--executor-memory $3G \
-	--py-files $HOME/BigDL/dist/lib/bigdl-0.11.0-SNAPSHOT-python-api.zip,$HOME/pipetune/applications/news20/textclassifier.py \
+	--py-files $HOME/BigDL/dist/lib/bigdl-0.12.0-SNAPSHOT-python-api.zip,$HOME/pipetune/apps/news20/textclassifier.py \
 	--properties-file $HOME/BigDL/dist/conf/spark-bigdl.conf \
-	--jars $HOME/BigDL/dist/lib/bigdl-0.11.0-SNAPSHOT-jar-with-dependencies.jar \
+	--jars $HOME/BigDL/dist/lib/bigdl-0.12.0-SNAPSHOT-jar-with-dependencies.jar \
         --conf spark.dynamicAllocation.enabled=false \
-	--conf spark.driver.extraClassPath=$HOME/BigDL/dist/lib/bigdl-0.11.0-SNAPSHOT-jar-with-dependencies.jar \
-	--conf spark.executer.extraClassPath=bigdl-0.11.0-SNAPSHOT-jar-with-dependencies.jar $HOME/pipetune/applications/news20/textclassifier.py \
+	--conf spark.driver.extraClassPath=$HOME/BigDL/dist/lib/bigdl-0.12.0-SNAPSHOT-jar-with-dependencies.jar \
+	--conf spark.executer.extraClassPath=bigdl-0.12.0-SNAPSHOT-jar-with-dependencies.jar $HOME/pipetune/apps/news20/textclassifier.py \
         --appName news20 \
 	--model $4 \
 	--maxEpoch 1 \
